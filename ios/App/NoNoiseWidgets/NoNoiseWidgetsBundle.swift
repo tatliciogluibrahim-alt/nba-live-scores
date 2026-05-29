88567bc5ef14e98f12022b0fcc45d827bb6a1bfd
tatliciogluibrahim-alt <tatlicioglu.ibrahim@gmail.com>
2026-05-28 22:38:08 -0400

    xcodestuff

---
+++ ios/App/NoNoiseWidgets/NoNoiseWidgetsBundle.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+import WidgetKit
+
+@main
+struct NoNoiseWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        NoNoiseLiveActivity()
+        // NoNoisePinnedWidget()   // Phase 22.5-4
+    }
+}
